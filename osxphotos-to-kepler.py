5449ec85e8d32162b362b96b1fe8f5ff3e0bbcb2
flintbison <>
2025-07-23 13:51:12 +0100

    speed of processing improve many-fold by removing all metadata properties except for filename, time and location.
    Will need to determine why so much processing overhead is associated with the other properties.

--- osxphotos-to-kepler.py
+++ osxphotos-to-kepler.py
@@ -5,10 +5,11 @@ import argparse
 import sys
 from tqdm import tqdm
 from yaspin import yaspin
+import math
 
 # Set up command-line argument parsing
-parser = argparse.ArgumentParser(description='Extract photo metadata and reformat date.')
-parser.add_argument('photosdb_path', help='Path to the Photos library database (e.g., ~/Pictures/Photos Library.photoslibrary)')
+parser = argparse.ArgumentParser(description='Extract photo metadata, sort by date and reformat date.')
+parser.add_argument('photosdb_path', help='Path to the Photos library database (e.g., "~/Pictures/Photos Library.photoslibrary")')
 args = parser.parse_args()
 
 # Output file path
@@ -25,7 +26,14 @@ try:
     with yaspin(text="Filtering and sorting photos...", color="cyan") as spinner:
         photos = [
             photo for photo in photosdb.photos()
-            if photo.date is not None and photo.location is not None
+            if photo.date is not None and
+               photo.location is not None and
+               isinstance(photo.location[0], (int, float)) and
+               isinstance(photo.location[1], (int, float)) and
+               not math.isnan(photo.location[0]) and
+               not math.isnan(photo.location[1]) and
+               -90 <= photo.location[0] <= 90 and
+               -180 <= photo.location[1] <= 180
         ]
         photos.sort(key=lambda x: x.date)
         spinner.ok("✔")
@@ -38,10 +46,8 @@ try:
     with open(output_file, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow([
-            "Filename", "DateTime", "Latitude", "Longitude", "Created", "Modified",
-            "HDR", "Edited", "MediaType", "Description", "Title",
-            "PlaceName", "PlaceAddress", "CameraMake", "CameraModel", "LensModel",
-            "Score", "Favourite"
+            "Filename", "DateTime", "Latitude", "Longitude"
+            # , "Description", "Title", "PlaceName", "CameraMake", "CameraModel", "LensModel", "Score", "Favourite"
         ])
         
         # Use tqdm for progress bar
@@ -49,30 +55,21 @@ try:
             filename = photo.original_filename
             # Reformat dates to yyyy-mm-dd hh:mm:ss to be compatible with kepler.gl
             datetime_str = photo.date.strftime("%Y-%m-%d %H:%M:%S") if photo.date else "N/A"
-            created_str = photo.date_added.strftime("%Y-%m-%d %H:%M:%S") if photo.date_added else "N/A"
-            modified_str = photo.date_modified.strftime("%Y-%m-%d %H:%M:%S") if photo.date_modified else "N/A"
             latitude = photo.location[0] if photo.location else "N/A"
             longitude = photo.location[1] if photo.location else "N/A"
-            hdr = photo.hdr
-            edited = bool(photo.hasadjustments)
-            media_type = photo.media_type if hasattr(photo, 'media_type') else "photo"
-            description = photo.description if photo.description else ""
-            title = photo.title if photo.title else ""
-            place_name = photo.place.name if photo.place and photo.place.name else "N/A"
-            place_address = photo.place.address if photo.place and photo.place.address else "N/A"
-            # Use exiftool.asdict() for camera metadata
-            exif_data = photo.exiftool.asdict() if photo.exiftool else {}
-            camera_make = exif_data.get('EXIF:Make', 'N/A')
-            camera_model = exif_data.get('EXIF:Model', 'N/A')
-            lens_model = exif_data.get('EXIF:LensModel', 'N/A')
-            score = photo.score.overall if photo.score and photo.score.overall else 0.0
-            favourite = photo.favorite
+            # title = photo.title if photo.title else ""
+            # description = photo.description if photo.description else ""
+            # # Use exiftool.asdict() for camera metadata
+            # exif_data = photo.exiftool.asdict() if photo.exiftool else {}
+            # camera_make = exif_data.get('EXIF:Make', 'N/A')
+            # camera_model = exif_data.get('EXIF:Model', 'N/A')
+            # lens_model = exif_data.get('EXIF:LensModel', 'N/A')
+            # score = photo.score.overall if photo.score and photo.score.overall else 0.0
+            # favourite = photo.favorite
             
             writer.writerow([
-                filename, datetime_str, latitude, longitude, created_str, modified_str,
-                hdr, edited, media_type, description, title,
-                place_name, place_address, camera_make, camera_model, lens_model,
-                score, favourite
+                filename, datetime_str, latitude, longitude
+                # , description, title, camera_make, camera_model, lens_model, score, favourite
             ])
     
     print(f"Processed metadata outputted to: {output_file}")
